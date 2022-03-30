e9ce510bbc832a5571e89cb9314876ebba96eee7
Imad Antonios <8104351+iantonios@users.noreply.github.com>
2022-03-29 23:14:22 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'csc330 spring 2021'
+app.config['SECRET_KEY'] = 'csc330 spring 2022'
 
 # dictionary of cities with their population
 cities = {}
